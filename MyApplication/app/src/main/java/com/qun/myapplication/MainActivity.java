767df17c41f256a15cb299f23082c3639c54c5ae
zoujuequn <151315539@qq.com>
2016-12-30 13:47:56 +0800

    1.首次提交

--- MyApplication/app/src/main/java/com/qun/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/com/qun/myapplication/MainActivity.java
@@ -2,7 +2,6 @@ package com.qun.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +9,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        TextView tv  = new TextView(this);
     }
 }
